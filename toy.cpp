7fa0bba69b7db4cdc271d98c099d62b66ff8628d
Yves Baumes <ybaumes@gmail.com>
2014-08-09 12:34:56 +0200

    Implement Elt constructor. Add more node instanciation.

--- toy.cpp
+++ toy.cpp
@@ -15,16 +15,22 @@ struct EltNode : public Node
 {
     string tag_name;
     map<string, string> attr;
+    EltNode() { type = Elt; }
 };
 
 struct TextNode : public Node
 {
     string data;
+    TextNode() { type = Text; }
 };
 
 int main()
 {
     cout << "hello toy.\n";
-    TextNode text1;
+    TextNode text1, text2;
     text1.data = "toto";
+    text2.data = "titi";
+    EltNode elt;
+    elt.children.push_back(&text1);
+    elt.children.push_back(&text2);
 }
